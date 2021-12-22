3f1b9535d7914239822a21ba305b99a871ddc0d4
Jayden Tavares <jayden.tavares@hotmail.com>
2021-12-23 01:00:10 +1100

    Create Entity and StateMachine

---
+++ src/entity/state_machine/state.gd
@@ -0,0 +1,28 @@
+class_name State
+extends Node
+
+var state_machine
+
+
+func _ready():
+	state_machine = get_parent()
+
+
+func enter():
+	pass
+
+
+func exit():
+	pass
+
+
+func process(_delta):
+	pass
+
+
+func physics_process(_delta):
+	pass
+
+
+func unhandled_input(_event):
+	pass
